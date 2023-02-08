e31ed3dd8e4ace64c4a06163c7beef5a07ee7ab5
Peteman03 <plw7203@outlook.com>
2023-02-08 15:46:07 -0500

    Got the birthday program working but the results seem off

--- src/Birthday.java
+++ src/Birthday.java
@@ -5,14 +5,18 @@ public class Birthday
 	public static void main(String [] args)
 	{
 		int people;
+		int runs;
 		Scanner reader = new Scanner(System.in);
 		
+		
+		System.out.println("How many runs would you like to do: ");
+		runs = reader.nextInt();
+		System.out.println("How many people per run: ");
 		people = reader.nextInt();
 		
-		for(int i = 0; i < people; i ++)
-		{
-		Person birthdays = new Person();
-		}
+		Person runner = new Person();
+		
+		System.out.println("The probablitiy of two people having the same birthday in a group of " + people + " people after " + runs + " runs is : " + runner.run(runs, people) + "%");
 	}
 	
 }
--- src/Person.java
+++ src/Person.java
@@ -8,7 +8,63 @@ public class Person
 	{
 		Random generator  = new Random();
 		
-		birthday = generator.nextInt(365);
+		birthday = generator.nextInt(365)+1;
 	}
 	
+	public int getBirthday()
+	{
+		return birthday;
+	}
+	
+	public double run(int runs, int people)
+	{
+		ArrayList<ArrayList<Person>> sampleSize = new ArrayList<ArrayList<Person>>();
+		
+		for(int i = 0; i < runs; i ++)
+		{
+			ArrayList<Person> group = new ArrayList<Person>();
+			for(int j = 0; j < people; j ++)
+			{
+				Person p = new Person(); 
+				group.add(p);
+			}
+			sampleSize.add(group);
+		}
+		
+		int counter = 0;
+		boolean check = false;
+		
+		for(int i = 0; i < sampleSize.size(); i ++)
+		{
+			for(int j = 0; j < sampleSize.get(i).size(); j ++)
+			{
+				for(int k = j+1; k < sampleSize.get(i).size(); k ++)
+				{
+					if(sampleSize.get(i).get(j).getBirthday() == sampleSize.get(i).get(k).getBirthday())
+					{
+						counter++;
+						check = true;
+						break;
+					}
+					if(check)
+					{
+						break;
+					}
+				}
+			}
+		}
+		
+		for(int i = 0; i < sampleSize.size(); i ++)
+		{
+			for(int j = 0; j < sampleSize.get(i).size(); j++)
+			{
+				System.out.print(sampleSize.get(i).get(j).getBirthday() + " ");
+			}
+			System.out.println();
+		}
+		
+		double results = (double)counter/runs * 100;
+		
+		return results;
+	}
 }
